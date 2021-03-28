0cd3ac6109d4139e3136bbc1d856dd1dfaa8cfb1
aastha2007 <76558877+aastha2007@users.noreply.github.com>
2021-03-28 19:59:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var hypnoticBall, database;
+var position;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  hypnoticBall = createSprite(250,250,10,10);
+  hypnoticBall.shapeColor = "red";
+
+/*
+.ref() is used to refer to the location of the database value.
+.on() creates a listener which keeps listening to the changes in the database.
+everytime a change in the database values of position(reference)happence,the readPosition function is called.
+if there is any error in reading the values in the database,the showError function is called.
+*/ 
+  var hypnoticBallPosition = database.ref('ball/position');
+  hypnoticBallPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+  background("white");
+  
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    drawSprites();
+  
+}
+
+//.set() is used to set the value in the database.
+function writePosition(x,y){
+  database.ref('ball/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readPosition(data){
+  position = data.val();
+  console.log(position.x);
+  hypnoticBall.x = position.x;
+  hypnoticBall.y = position.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
+
